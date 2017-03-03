633dbac3e692d366f1b8ed63655259bdf2f93f0b
lummie <lummie.co.uk@gmail.com>
2017-03-03 04:29:15 -0800

    Added general Equal method

--- assert.go
+++ assert.go
@@ -80,3 +80,38 @@ func Len(a, b interface{}) bool {
 	}
 	panic("Value parameter not an Array")
 }
+
+
+// Checks that the supplied expected and actual objects are equal
+// this code is a copy of the ObjectsAreEqual method from :
+// 		https://github.com/stretchr/testify/blob/master/assert/assertions.go
+//		Copyright (c) 2012 - 2013 Mat Ryer and Tyler Bunnell
+func Equal(expected, actual interface{}) bool {
+
+	if expected == nil || actual == nil {
+		return expected == actual
+	}
+
+	if reflect.DeepEqual(expected, actual) {
+		return true
+	}
+
+	expectedValue := reflect.ValueOf(expected)
+	actualValue := reflect.ValueOf(actual)
+
+	if expectedValue == actualValue {
+		return true
+	}
+
+	// Attempt comparison after type conversion
+	if actualValue.Type().ConvertibleTo(expectedValue.Type()) && expectedValue == actualValue.Convert(expectedValue.Type()) {
+		return true
+	}
+
+	// Last ditch effort
+	if fmt.Sprintf("%#v", expected) == fmt.Sprintf("%#v", actual) {
+		return true
+	}
+
+	return false
+}
